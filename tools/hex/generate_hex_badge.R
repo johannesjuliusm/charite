48a2af415980536db496bb8272624a4d4013b3a5
Johannes Julius Mohn <johannes.julius@outlook.com>
2025-04-20 02:51:49 +0200

    Updated hex sticker and supporting files

--- tools/hex/generate_hex_badge.R
+++ tools/hex/generate_hex_badge.R
@@ -5,13 +5,13 @@ library(ggplot2)
 library(charite)
 
 sticker(
-  subplot = "inst/221215_charite_bildmarke_rgb.png",
+  subplot = "inst/bettenhochhaus.png",
   package = "charite",
   p_size = 16,
   s_x = 1,         # center horizontally
   s_y = 0.9,         # slightly lower if needed
-  s_width = 0.5,   # smaller image
-  s_height = 0.5,  # optional, keeps proportions clean
+  s_width = 0.45,   # smaller image
+  s_height = 0.45,  # optional, keeps proportions clean
   h_fill = "#ffffff",
   h_color = "#004d9b",
   p_color = "#7e898f",
